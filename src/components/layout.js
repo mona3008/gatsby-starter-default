25e94a0e77e1e7a12a8b59d49e330da8c052a62e
mona3008 <ramonastreiner@icloud.com>
2022-11-22 17:38:29 -0500

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,59 +1,51 @@
 /**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
+  * Layout component that queries for data
+  * with Gatsby's useStaticQuery component
+  *
+  * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
+  */
 
-import * as React from "react"
+ import * as React from "react"
+ import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+ import Header from "./header"
+ import "./layout.css"
 
-const Layout = ({ pageTitle, children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+ const Layout = ({ children }) => {
+   const data = useStaticQuery(graphql`
+     query SiteTitleQuery {
+       site {
+         siteMetadata {
+           title
+         }
+       }
+     }
+   `)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div>
-      <nav>
-      <ul>
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
+   return (
+     <>
+       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+       <div
+         style={{
+           margin: `0 auto`,
+           maxWidth: `var(--size-content)`,
+           padding: `var(--size-gutter)`,
+         }}
+       >
+         <main>{children}</main>
+         <footer
+           style={{
+             marginTop: `var(--space-5)`,
+             fontSize: `var(--font-sm)`,
+           }}
+         >
+           © {new Date().getFullYear()} &middot; Built with
+           {` `}
+           <a href="https://www.gatsbyjs.com">Gatsby</a>
+         </footer>
+       </div>
+     </>
+   )
+ }
 
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li
-        <main>
-        <h1>{pageTitle}</h1>
-        {children}
-        </main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-         </ul>
-      </nav>
-      </div>
-    </>
-  )
-}
-
-export default Layout
+ export default Layout
